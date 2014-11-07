a972c5ec8d2f5ea74e798581aec058bee922e399
Jussi Mertanen <juzax@hotmail.com>
2014-11-07 11:27:16 +0200

    Fixed login cucumber tests

--- features/login.feature
+++ features/login.feature
@@ -3,18 +3,19 @@ Feature: Login
 
     Scenario: Login with correct credentials
         Given I am on the login page
-	And I fill in the correct username and password
-	When I press Sign in
-	Then I should see a success message
+        And There exists a user
+	    And I fill in the correct username and password
+	    When I press Sign in
+	    Then I should see a success message
 
     Scenario: Login with incorrect password
-	Given I am on the login page
-	And I fill in an incorrect password
-	When I press Sign in
-	Then I should see a fail message    
+	    Given I am on the login page
+	    And I fill in an incorrect password
+	    When I press Sign in
+	    Then I should see a fail message
 
     Scenario: Login with incorrect username
-	Given I am on the login page
-	And I fill in an incorrect username
-	When I press Sign in
-	Then I should see a fail message    
+	    Given I am on the login page
+	    And I fill in an incorrect username
+	    When I press Sign in
+	    Then I should see a fail message
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -2,17 +2,21 @@ Given /^I am on the login page$/ do
     visit new_user_session_path
 end
 
+And /^There exists a user$/ do
+  User.create(username: 'pena', password: 'penapena', password_confirmation: 'penapena')
+end
+
 And /^I fill in the correct username and password$/ do
-    fill_in('user_username', with: 'username')
-    fill_in('user_password', with: 'password')
+    fill_in('user_username', with: 'pena')
+    fill_in('user_password', with: 'penapena')
 end
 
-When /^I press Sign in$/
-    click_on('Sign in')
+When /^I press Sign in$/ do
+    click_button('Sign in')
 end
 
-Then /^I should see a success message$/
-    expect(page).to have_content('')
+Then /^I should see a success message$/ do
+    expect(page).to have_content('Signed in successfully.')
 end
 
 And /^I fill in an incorrect password$/ do
@@ -25,7 +29,7 @@ And /^I fill in an incorrect username$/ do
     fill_in('user_password', with: 'password')
 end
 
-Then /^I should see a fail message$/
+Then /^I should see a fail message$/ do
     expect(page).to have_content('Invalid username or password')
 end
 
