c9b8d0a267cf5313c57e83eaab2b8e0861592b35
Juho Leinonen <messionyk@gmail.com>
2014-11-07 13:07:38 +0200

    Tests for image and image_collection

--- spec/models/image_collection_spec.rb
+++ spec/models/image_collection_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ImageCollection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:image_collection) { FactoryGirl.create(:image_collection) }
+  it { is_expected.to be_valid }
 end
--- spec/views/images/index.html.erb_spec.rb
+++ spec/views/images/index.html.erb_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe 'images/index', type: :view do
         image_collection_id: 1,
         size: 'Size',
         is_private: false,
-        latitude: '',
-        longitude: ''
+        latitude: '60.2',
+        longitude: '24.5'
       ),
       Image.create!(
         image_collection_id: 1,
         size: 'Size',
         is_private: false,
-        latitude: '',
-        longitude: ''
+        latitude: '60.2',
+        longitude: '24.5'
       )
     ])
   end
@@ -25,7 +25,7 @@ RSpec.describe 'images/index', type: :view do
     assert_select 'tr>td', text: 1.to_s, count: 2
     assert_select 'tr>td', text: 'Size'.to_s, count: 2
     assert_select 'tr>td', text: false.to_s, count: 2
-    assert_select 'tr>td', text: ''.to_s, count: 2
-    assert_select 'tr>td', text: ''.to_s, count: 2
+    assert_select 'tr>td', text: '60.2'.to_s, count: 2
+    assert_select 'tr>td', text: '24.5'.to_s, count: 2
   end
 end
