1c8a71d2b50260be8e9a5b7ccdf9b637a9b05d54
Jussi Mertanen <juzax@hotmail.com>
2014-11-20 17:56:10 +0200

    Started final visualizations

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,24 @@ class Image < ActiveRecord::Base
   
   belongs_to :collection
   has_one :subject, through: :collection
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |image|
+        csv << image.attributes.values_at(*column_names)
+      end
+    end
+  end
+
+  def self.average_image_file_age
+    number_of_images = Image.all.count
+    average_age = 0
+    Image.all.each do |image|
+      average_age = average_age + image.date.to_time.to_i - image.date_taken.to_time.to_i
+    end
+    average_age = average_age/number_of_images
+    # Convert seconds into hours
+    average_age/86400
+  end
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -3,4 +3,24 @@ class Text < ActiveRecord::Base
 
   belongs_to :collection
   has_one :subject, through: :collection
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |image|
+        csv << image.attributes.values_at(*column_names)
+      end
+    end
+  end
+
+  def self.average_text_file_age
+    number_of_texts = Text.all.count
+    average_age = 0
+    Text.all.each do |text|
+      average_age = average_age + (text.date.to_time.to_i - text.date_added.to_time.to_i)
+    end
+    average_age = average_age/number_of_texts unless number_of_texts == 0
+    # Convert seconds into hours
+    average_age/86400
+  end
 end
