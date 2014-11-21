ae2eb239b2304e07d7a403160f09d241847613c0
Henrik Nygren <nygrenh@gmail.com>
2014-11-21 14:58:38 +0200

    Fix cucumber

--- features/step_definitions/export_steps.rb
+++ features/step_definitions/export_steps.rb
@@ -1,6 +1,7 @@
 Then /^I can export a csv file$/ do
   click_link('CSV')
-  expect(page.html).to have_content('abc,def,,ghi,jkl,UID123')
+  expect(page.html).to have_content('abc,def,,ghi,jkl')
+  expect(page.html).not_to have_content('UID123')
 end
 
 Then /^I can export an xls file$/ do
