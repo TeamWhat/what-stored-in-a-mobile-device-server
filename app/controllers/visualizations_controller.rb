52b594f33875120ac029997fd1ab861094472178
Jussi Mertanen <juzax@hotmail.com>
2014-11-21 12:24:49 +0200

    Fix average file age bars

--- app/controllers/visualizations_controller.rb
+++ app/controllers/visualizations_controller.rb
@@ -1,5 +1,7 @@
 class VisualizationsController < ApplicationController
   def index
     @brand_count = Subject.group(:brand).count
+    @average_text_file_age = Text.average_text_file_age
+    @average_image_file_age = Image.average_image_file_age
   end
 end
