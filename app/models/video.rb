a0b3c97c76c39e72fa1de943d91c5278351adff3
Juho Leinonen <messionyk@gmail.com>
2014-11-25 11:18:26 +0200

    Add info to xls, add video xls

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,13 +1,6 @@
 class Video < ActiveRecord::Base
+  extend Csvable
+
   belongs_to :collection
   has_one :subject, through: :collection
-
-  def self.to_csv(options = {})
-    CSV.generate(options) do |csv|
-      csv << column_names
-      all.each do |video|
-        csv << video.attributes.values_at(*column_names)
-      end
-    end
-  end
 end
