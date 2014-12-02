e416f18ff7b6acb356594c79cd34f81d7666cacd
Henrik Nygren <nygrenh@gmail.com>
2014-12-02 17:13:51 +0200

    Refactored statistics methods

--- app/models/concerns/statable.rb
+++ app/models/concerns/statable.rb
@@ -9,11 +9,62 @@ module Statable
     size_count
   end
 
-  def total_count
-    table.all.count
+  def average_age
+    average(:time_difference, 86_400)
   end
+
+  def average_size
+    average(:size, 1000)
+  end
+
+  def maximum_age
+    extreme_value(:time_difference, :max_by, 86_400)
+  end
+
+  def minimum_age
+    extreme_value(:time_difference, :min_by, 86_400)
+  end
+
+  def minimum_size
+    extreme_value(:size, :min_by, 1000)
+  end
+
+  def maximum_size
+    extreme_value(:size, :max_by, 1000)
+  end
+
+  private
 
   def table
     self.class.const_get(name)
+  end
+
+  def extreme_value(calculation, sort, divider)
+    return if table.all.empty?
+    extreme = table.all.send sort do |o|
+      send(calculation, o)
+    end
+    send(calculation, extreme) / divider
+  end
+
+  def average(calculation, divider)
+    average = 0
+    table.all.each do |o|
+      average += send(calculation, o)
+    end
+    average /= total_count unless total_count == 0
+    average / divider
+  end
+
+  def time_difference(o)
+    o.date.to_time.to_i - o.date_added.to_time.to_i
+  end
+
+  def size(o)
+    o.size
+  end
+
+  def total_count
+    table.all.count
   end
 end
